7e23f941062691e0edbc9418b3f0d72a64591333
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-07-29 12:25:01 -0700

    Modify .gitignore

---
+++ Qiskit-aqua_tutorials/Finance/My_first_finance_experiment.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 11:53:17 2020
+
+@author: Gonzalo
+
+Evaluate a fixed income asset with undertain interest rates using Amplitude estimation.
+"""
+
+import numpy as np
+import qiskit as q
